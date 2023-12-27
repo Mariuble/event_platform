48a22abd7cd15335df55fac3f1512dd5bd54a864
mariuble <54665810+Mariuble@users.noreply.github.com>
2023-12-27 23:00:10 +0100

    big commit

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -2,19 +2,20 @@ import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-let cached = (global as any).mongoose || { connect: null, promise: null };
+let cached = (global as any).mongoose || { conn: null, promise: null };
 
 // Cached pattern for serverless environment to efficiently manage connection in separate processes
 export const connectToDatabase = async () => {
-    if (cached.conn) return cached.conn;
+  if (cached.conn) return cached.conn;
 
-    if (!MONGODB_URI) throw new Error('MONGODB_URI is missing');
+  if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
 
-    cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
-        dbName: 'evently',
-        bufferCommands: false,
-    })
+  cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
+    dbName: 'evently',
+    bufferCommands: false,
+  })
 
-    cached.conn = await cached.promise;
-    return cached.conn;
+  cached.conn = await cached.promise;
+
+  return cached.conn;
 }
